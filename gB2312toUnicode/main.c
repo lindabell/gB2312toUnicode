a77f686d3559308fdac95d91c523421bee527863
lindabell <lindabell@163.com>
2013-04-02 10:03:34 +0800

    fix funtion of main example BUG

--- gB2312toUnicode/main.c
+++ gB2312toUnicode/main.c
@@ -1,12 +1,13 @@
 #include "stdio.h"
 #include "unicodeToGB2312.h"
+
 void main(void)
 { 
 	u16 unicode_temp;
 
 	printf("Unicode\t\tGB2312\n");
 
-	unicode_temp=GB2312ToUnicode('֣');
+	unicode_temp=GB2312ToUnicode(0xD6A3);	//郑的GB2312吗是0xD6A3  unicode是0x90D1
 	printf("%02X\t\t",unicode_temp);
 
 	unicode_temp=UnicodeToGB2312(unicode_temp);
